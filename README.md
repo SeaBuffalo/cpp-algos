7b7b34d7b67485b96770241590074aad67e1cbf5
Ben Schulze <benschulze@Bens-Air.attlocal.net>
2024-02-25 08:36:52 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cpp-algos
+A collection of basic algorithms written to help me learn cpp
---
+++ quicksort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> quicksort(std::vector<int> arr) {
+  
+} 
